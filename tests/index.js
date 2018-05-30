0b211789f001080cc2bfe5d55300e152d873d341
zyevegor <zyev.egor@gmail.com>
2018-05-30 22:15:38 +0300

    https://chronobank.atlassian.net/browse/MD-368 - update docker files

--- tests/index.js
+++ tests/index.js
@@ -71,12 +71,12 @@ describe('core/2fa', function () {
 
     expect(ctx.users.userFrom.balance.toNumber()).to.eq(0);
     expect(ctx.users.userTo.balance.toNumber()).to.eq(0);
-    expect(ctx.users.owner.balance.toNumber()).to.be.above(parseInt(ctx.users.owner.web3.toWei(2, 'ether')));
+    expect(ctx.users.owner.balance.toNumber()).to.be.above(parseInt(ctx.users.owner.web3.toWei(6, 'ether')));
 
   });
 
-  it('sending 2 ethers to userFrom from owner', async () => {
-    const userFromTransferAmount = ctx.users.userFrom.web3.toWei(2, 'ether');
+  it('sending 6 ethers to userFrom from owner', async () => {
+    const userFromTransferAmount = ctx.users.userFrom.web3.toWei(6, 'ether');
     let userFromTranserTx = await Promise.promisify(ctx.users.owner.web3.eth.sendTransaction)({
       to: ctx.users.userFrom.wallet.getAddressString(),
       value: userFromTransferAmount,
@@ -104,7 +104,7 @@ describe('core/2fa', function () {
 
   it('validating userFrom balance', async () => {
     ctx.users.userFrom.balance = await Promise.promisify(ctx.users.userFrom.web3.eth.getBalance)(ctx.users.userFrom.wallet.getAddressString());
-    expect(ctx.users.userFrom.balance.toNumber()).to.eq(parseInt(ctx.users.owner.web3.toWei(2, 'ether')));
+    expect(ctx.users.userFrom.balance.toNumber()).to.eq(parseInt(ctx.users.owner.web3.toWei(6, 'ether')));
 
   });
 
@@ -318,16 +318,159 @@ describe('core/2fa', function () {
       json: true
     });
 
-    const walletLog = _.find(confirmedTx.logs, {event: 'MultisigWalletMultiTransact'});
-    expect(walletLog).to.be.an('object');
+    const latestFilter = ctx.users.userFrom.web3.eth.filter('latest');
+
+    await new Promise(res => {
+      latestFilter.watch(async function (error) {
+        if (error)
+          return;
+        let tx = await Promise.promisify(ctx.users.userFrom.web3.eth.getTransaction)(confirmedTx.hash);
+        if (tx.blockNumber) {
+          await new Promise(res => latestFilter.stopWatching(res));
+          res();
+        }
+      });
+    });
   });
 
   it('validate balance', async () => {
-    await Promise.delay(10000);
-
     const transferAmount = ctx.users.userTo.web3.toWei(1, 'ether');
     const userToBalance = await Promise.promisify(ctx.users.userTo.web3.eth.getBalance)(ctx.users.userTo.wallet.getAddressString());
     expect(userToBalance.toNumber()).to.eq(parseInt(transferAmount));
   });
+
+  it('create second wallet', async () => {
+
+    ctx.contracts.WalletsManager.setProvider(ctx.users.userFrom.web3.currentProvider);
+    const walletsManager = await ctx.contracts.WalletsManager.deployed();
+
+    const walletCreationEstimateGasPrice = await walletsManager.create2FAWallet.estimateGas(0);
+
+    let createWalletTx = await walletsManager.create2FAWallet(0, {
+      from: ctx.users.userFrom.wallet.getAddressString(),
+      gas: parseInt(walletCreationEstimateGasPrice * 1.5)
+    });
+
+    expect(createWalletTx.tx).to.be.a('string');
+
+    const latestFilter = ctx.users.userFrom.web3.eth.filter('latest');
+
+    await new Promise(res => {
+      latestFilter.watch(async function (error) {
+        if (error)
+          return;
+        let tx = await Promise.promisify(ctx.users.userFrom.web3.eth.getTransaction)(createWalletTx.tx);
+        if (tx.blockNumber) {
+          await new Promise(res => latestFilter.stopWatching(res));
+          res();
+        }
+      });
+    });
+
+    const walletLog = _.find(createWalletTx.logs, {event: 'WalletCreated'});
+    expect(walletLog).to.be.an('object');
+  });
+
+  it('transfer to second wallet', async () => {
+    await Promise.delay(30000);
+
+    ctx.contracts.Wallet.setProvider(ctx.users.userFrom.web3.currentProvider);
+    const transferAmount = ctx.users.userFrom.web3.toWei(3, 'ether');
+
+    let walletList = await request({
+      uri: `http://localhost:${config.rest.port}/wallets/${ctx.users.userFrom.wallet.getAddressString()}`,
+      json: true
+    });
+
+    let wallet = ctx.contracts.Wallet.at(walletList[1].address);
+
+    await Promise.promisify(ctx.users.userFrom.web3.eth.sendTransaction)({
+      to: walletList[1].address,
+      value: transferAmount,
+      gas: 50000,
+      from: ctx.users.userFrom.wallet.getAddressString()
+    });
+
+    let walletsManager = await ctx.contracts.WalletsManager.deployed();
+    const price = await walletsManager.getOraclePrice();
+
+    const walletTransferEstimateGasPrice = await wallet.transfer.estimateGas(ctx.users.userTo.wallet.getAddressString(), transferAmount, 'ETH', {
+      value: price,
+      from: ctx.users.userFrom.wallet.getAddressString()
+    });
+
+    let transferTx = await wallet.transfer(ctx.users.userTo.wallet.getAddressString(), transferAmount, 'ETH', {
+      value: price,
+      from: ctx.users.userFrom.wallet.getAddressString(),
+      gas: parseInt(walletTransferEstimateGasPrice * 1.2)
+    });
+
+    expect(transferTx.tx).to.be.a('string');
+
+    const walletLog = _.find(transferTx.logs, {event: 'MultisigWalletConfirmationNeeded'});
+    expect(walletLog).to.be.an('object');
+
+    const latestFilter = ctx.users.userFrom.web3.eth.filter('latest');
+
+    await new Promise(res => {
+      latestFilter.watch(async function (error) {
+        if (error)
+          return;
+        let tx = await Promise.promisify(ctx.users.userFrom.web3.eth.getTransaction)(transferTx.tx);
+        if (tx.blockNumber) {
+          await new Promise(res => latestFilter.stopWatching(res));
+          res();
+        }
+      });
+    });
+  });
+
+  it('confirm', async () => {
+    await Promise.delay(30000);
+
+    let walletList = await request({
+      uri: `http://localhost:${config.rest.port}/wallets/${ctx.users.userFrom.wallet.getAddressString()}`,
+      json: true
+    });
+
+    let walletWithPendingOperation = _.find(walletList, wallet => wallet.operations.length);
+
+    let token = speakeasy.totp({ //client side
+      secret: ctx.users.userFrom.secret,
+      encoding: 'base32'
+    });
+
+    const confirmedTx = await request({
+      method: 'POST',
+      uri: `http://localhost:${config.rest.port}/wallet/confirm`,
+      body: {
+        token: token,
+        operation: walletWithPendingOperation.operations[0],
+        wallet: walletWithPendingOperation.address
+      },
+      json: true
+    });
+
+    const latestFilter = ctx.users.userFrom.web3.eth.filter('latest');
+
+    await new Promise(res => {
+      latestFilter.watch(async function (error) {
+        if (error)
+          return;
+        let tx = await Promise.promisify(ctx.users.userFrom.web3.eth.getTransaction)(confirmedTx.hash);
+        if (tx.blockNumber) {
+          await new Promise(res => latestFilter.stopWatching(res));
+          res();
+        }
+      });
+    });
+  });
+
+  it('validate balance again', async () => {
+    const transferAmount = ctx.users.userTo.web3.toWei(4, 'ether');
+    const userToBalance = await Promise.promisify(ctx.users.userTo.web3.eth.getBalance)(ctx.users.userTo.wallet.getAddressString());
+    expect(userToBalance.toNumber()).to.eq(parseInt(transferAmount));
+  });
+
 
 });
