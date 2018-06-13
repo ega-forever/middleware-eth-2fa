bef3ed9f92c8e9262ec2a771326a51e851913da1
egor <zyev.egor@rambler.ru>
2018-06-13 20:27:57 +0300

    https://chronobank.atlassian.net/browse/MD-399 - middleware eth 2fa - eth - ошибка в wallet/confirm

--- migrations/tabs.js
+++ migrations/tabs.js
@@ -1,5 +1,5 @@
 
-module.exports.id = '28.tabs';
+module.exports.id = 'tabs';
 
 const _ = require('lodash'),
   config = require('../config');
@@ -12,12 +12,12 @@ const _ = require('lodash'),
 
 module.exports.up = function (done) {
   let coll = this.db.collection(`${_.get(config, 'nodered.mongo.collectionPrefix', '')}noderedstorages`);
-  coll.update({'path':'tabs','type':'flows'}, {
-    $set: {'path':'tabs','body':[{'id':'187463b9.50a76c','type':'tab','label':'events','disabled':false,'info':''},{'id':'7529dc22.ee9104','type':'tab','label':'rest','disabled':false,'info':''}]}
+  coll.update({"path":"tabs","type":"flows"}, {
+    $set: {"path":"tabs","body":[{"id":"187463b9.50a76c","type":"tab","label":"events","disabled":false,"info":""},{"id":"7529dc22.ee9104","type":"tab","label":"rest","disabled":false,"info":""}]}
   }, {upsert: true}, done);
 };
 
 module.exports.down = function (done) {
   let coll = this.db.collection(`${_.get(config, 'nodered.mongo.collectionPrefix', '')}noderedstorages`);
-  coll.remove({'path':'tabs','type':'flows'}, done);
+  coll.remove({"path":"tabs","type":"flows"}, done);
 };
